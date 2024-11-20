747bd21118a9a3f23c682b002706e85d5ce1d9a5
BeTaJI <lilysh994@mail.ru>
2024-11-20 21:29:46 +0300

    добавил хук локал стораджа

---
+++ src/hooks/useLocalStorage/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+import { Node } from 'types/index';
+
+export const useLocalStorage = (key: string, defaultValue?: Node[]) => {
+  const [value, setValue] = useState<Node[]>(() => {
+    const jsonValue = localStorage.getItem(key);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+    if (defaultValue) return defaultValue;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+};
