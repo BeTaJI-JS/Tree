392a5289c664a18f31b4ed16f8a3ab450985af65
BeTaJI <lilysh994@mail.ru>
2024-11-22 12:31:29 +0300

    сломал логику на добавление в корень и развертывание дерева. откатываю до стабильной версии точно фискс локал стора оставлю

--- src/hooks/useLocalStorage/index.tsx
+++ src/hooks/useLocalStorage/index.tsx
@@ -5,7 +5,8 @@ import { Node } from 'types/index';
 export const useLocalStorage = (key: string, defaultValue?: Node[]) => {
   const [value, setValue] = useState<Node[]>(() => {
     const jsonValue = localStorage.getItem(key);
-    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    if (jsonValue !== null && !jsonValue.length) return JSON.parse(jsonValue);
     if (defaultValue) return defaultValue;
   });
 
