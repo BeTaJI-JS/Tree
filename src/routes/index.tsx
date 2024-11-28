f054b288f21339984dfa397f5bb4a93b5942d189
BeTaJI <lilysh994@mail.ru>
2024-11-28 17:26:32 +0300

    fix warnings router v7

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,20 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Content from 'components/Content';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: <Content />,
+      path: '/',
+    },
+  ],
   {
-    element: <Content />,
-    path: '/',
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
   },
-]);
+);
