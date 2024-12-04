e396842dc543553fabc5e6add692961d22091b8e
BeTaJI <lilysh994@mail.ru>
2024-12-04 13:32:34 +0300

    обновил логику хука

--- src/hooks/useWebsocket.ts
+++ src/hooks/useWebsocket.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Message } from 'types';
 
@@ -7,59 +7,62 @@ const useWebSocket = (url: string) => {
   const [error, setError] = useState<string | null>(null);
   const [isConnected, setIsConnected] = useState(false);
   const socketRef = useRef<WebSocket | null>(null);
-  console.log('socketRef', socketRef);
-
-  const connect = useCallback(() => {
-    if (socketRef.current) {
-      // Если сокет уже существует, не создаем новый
-      return;
-    }
-
-    socketRef.current = new WebSocket(url);
-
-    socketRef.current.onopen = () => {
-      setIsConnected(true);
-      setError(null);
-    };
-
-    socketRef.current.onmessage = (event) => {
-      const newMessage = JSON.parse(event.data);
-      setMessages((prevMessages) => {
-        // Сравниваем предыдущие сообщения с новыми
-        if (JSON.stringify(prevMessages[prevMessages.length - 1]) !== JSON.stringify(newMessage)) {
-          return [...prevMessages, newMessage];
-        }
-        return prevMessages;
-      });
-    };
-
-    socketRef.current.onerror = (event) => {
-      console.log('event', event);
-
-      if (event instanceof Error) {
-        setError(event.message);
-      } else {
-        setError(null);
-      }
-    };
-    socketRef.current.onclose = () => {
-      setIsConnected(false);
-      // Попробуем переподключиться через 2 секунды
-      setTimeout(() => {
-        connect();
-      }, 2000);
-    };
-  }, [url]);
 
   useEffect(() => {
-    connect();
+    const wsConnection = socketRef.current;
+
+    const connectWebSocket = () => {
+      if (!wsConnection) {
+        return;
+      }
+      socketRef.current = new WebSocket(url);
+
+      wsConnection.onopen = () => {
+        setIsConnected(true);
+        setError(null);
+      };
+
+      wsConnection.onmessage = (event) => {
+        try {
+          const newMessage: Message = JSON.parse(event.data);
+          setMessages((prevMessages) => {
+            // Сравниваем предыдущие сообщения с новыми
+            if (
+              prevMessages.length === 0 ||
+              JSON.stringify(prevMessages[prevMessages.length - 1]) !== JSON.stringify(newMessage)
+            ) {
+              return [...prevMessages, newMessage];
+            }
+            return prevMessages;
+          });
+        } catch (err) {
+          console.error('Error parsing message:', err);
+        }
+      };
+
+      wsConnection.onerror = (event) => {
+        if (event instanceof Error) {
+          setError(event.message);
+        } else {
+          setError('Неизвестная ошибка');
+        }
+      };
+
+      wsConnection.onclose = () => {
+        setIsConnected(false);
+        // Пробуем переподключиться через 2 секунды
+        setTimeout(connectWebSocket, 2000);
+      };
+    };
+
+    connectWebSocket();
 
     return () => {
-      if (socketRef.current?.readyState === WebSocket.OPEN) {
-        socketRef.current.close();
+      if (wsConnection) {
+        wsConnection.close();
       }
     };
-  }, [connect]);
+  }, [url]);
 
   return { messages, error, isConnected };
 };
